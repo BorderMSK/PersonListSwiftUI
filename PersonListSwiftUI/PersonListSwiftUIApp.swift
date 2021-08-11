9c7f43bc8afccc84eb149ed0d6d26bbd2c946f24
Igor Makeev <makeevigor@mail.ru>
2021-08-11 15:16:53 +0300

    Initial Commit

---
+++ PersonListSwiftUI/PersonListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonListSwiftUIApp.swift
+//  PersonListSwiftUI
+//
+//  Created by Igor Makeev on 11.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct PersonListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
